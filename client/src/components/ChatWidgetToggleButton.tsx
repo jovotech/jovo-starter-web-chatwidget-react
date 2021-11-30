75d6e796c95bf1063ee7171738cdf8999303d9ad
Max Ripper <max@jovo.tech>
2021-11-30 20:43:26 +0100

    :lipstick: Implement transition for toggle button

--- client/src/components/ChatWidgetToggleButton.tsx
+++ client/src/components/ChatWidgetToggleButton.tsx
@@ -1,5 +1,6 @@
 import React, { VFC } from 'react';
 import { cls } from '../utils';
+import { Transition } from '@headlessui/react';
 
 export interface ChatWidgetToggleButtonProps {
   isWindowVisible: boolean;
@@ -17,7 +18,7 @@ const ChatWidgetToggleButton: VFC<ChatWidgetToggleButtonProps> = (props) => {
       )}
       onClick={props.onClick}
     >
-      {!props.isWindowVisible ? (
+      <Transition show={!props.isWindowVisible} enter="transition-opacity duration-150" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity duration-150 absolute" leaveFrom="opacity-100" leaveTo="opacity-0">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-8 w-8"
@@ -32,9 +33,9 @@ const ChatWidgetToggleButton: VFC<ChatWidgetToggleButtonProps> = (props) => {
             d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
           />
         </svg>
-      ) : (
+      </Transition>
+      <Transition show={props.isWindowVisible} enter="transition-opacity duration-150" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity duration-150 absolute" leaveFrom="opacity-100" leaveTo="opacity-0">
         <svg
-          key="active"
           xmlns="http://www.w3.org/2000/svg"
           className="h-8 w-8"
           fill="none"
@@ -43,7 +44,21 @@ const ChatWidgetToggleButton: VFC<ChatWidgetToggleButtonProps> = (props) => {
         >
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
         </svg>
-      )}
+      </Transition>
+      {/*{!props.isWindowVisible ? (*/}
+
+      {/*) : (*/}
+      {/*  <svg*/}
+      {/*    key="active"*/}
+      {/*    xmlns="http://www.w3.org/2000/svg"*/}
+      {/*    className="h-8 w-8"*/}
+      {/*    fill="none"*/}
+      {/*    viewBox="0 0 24 24"*/}
+      {/*    stroke="currentColor"*/}
+      {/*  >*/}
+      {/*    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />*/}
+      {/*  </svg>*/}
+      {/*)}*/}
     </button>
   );
 };
